3f54b86316369a5b67cbd5032d71aef38962d7d3
ktpgod69 <henry.fuentes0624@gmail.com>
2024-01-23 19:19:47 -0600

    variables

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("What has been your favorite thing when it comes to teaching?");
+let life = 90; // this is my life bar number
+        
+life = life - 30; 
+
+//60
+
+ life = life - 20;
+
+//40
+ life = "ktp dev"; // Name displayed in game string
+
+ life = true; // Boolean
+
+ life = 40;
+
+//object synbull
+
+
+
+
+console.log(life);
