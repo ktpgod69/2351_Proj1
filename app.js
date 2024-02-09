4df1cbbee8a034a4f63fe6789a42a4424e817dfc
ktpgod69 <henry.fuentes0624@gmail.com>
2024-02-08 22:44:52 -0600

    strings

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
-const name = "AEG Designs";
-const gamer = "Ktp Streaks";
+const name = "Henry";
+const age = 30;
+const member = 34;
 
-function adder(num1, num2) {
-console.log(num1 + num2);
-console.log(num1 - num2);
+if (member > 25) {
+    console.log("welcome subscriber!");
+ } else {
+    console.log("goodbye subscriber");
+ }
+console.log(`Welcome to AEG Design Im ${name} and im ${age}`);
+const combined = name + age;
+
+console.log(typeof age);
+console.log(typeof combined);
+
+const member1 = 75;
+const member2 = 20;
+
+if (member1 === 75 && member2 === 20) {
+    console.log("higher sales");
+} else {
+    console.log("lowest sales");
 }
-
-
-function toUpper(text) {
-    const upperCased = text.toUpperCase();
-    console.log(upperCased);
-}
-
-function logger() {
-    
-console.log("Gaming Sesh");
-console.log("Gaming Sesh");
-console.log("Gaming Sesh");
-console.log("Gaming Sesh");
-}
-
-
-
-logger();
-adder(35, 9);
-adder(10, 7);
-adder(35, 12);
-toUpper(name);
-toUpper(gamer);
-
 
 
