21b3c21384641649a06398fdc35ac6086f612c56
ktpgod69 <henry.fuentes0624@gmail.com>
2024-02-17 22:19:05 -0600

    ok

--- app.js
+++ app.js
@@ -16,3 +16,35 @@ while(loading < 80) {
     loading++;
 }
 
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
+
+text.style.color = "teal"
+text.style.backgroundColor = "pink";
+
+text.classList.add("change");
+
+changeColor.addEventListener("click", function() {
+    text.classList.toggle("change");
+});
+const userlist = document.querySelectorAll(".name-list");
+const listinput = document.querySelector(".listinput");
+const addListBtn = document.querySelector(".addListBtn");
+
+addListBtn.addEventListener("click", function() {
+//Create an Li out of thin air
+const newLi = document.createElement("Li");
+const liContent = document.createTextNode("listInput.value");
+//Add the input value inside that new Li
+newLi.appendChild(licontent);
+//Attaching the Li to the user list
+userlist.appendChild(newLi);
+});
+
+for (user of userList) {
+    user.addEventListener("click", function() {
+        this.style.color = "pink";
+        console.log(this);
+      });
+    }
+
