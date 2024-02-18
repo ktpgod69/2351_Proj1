8c98549900460f9091200c4058e3bb6929784ddf
ktpgod69 <henry.fuentes0624@gmail.com>
2024-02-17 19:55:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-const name = "Henry";
-const age = 30;
-const member = 34;
+const names = ["Levi", "Olivia", "Ellisa", "Priscilla", "George", "Robert", "Rj"];
 
-if (member > 25) {
-    console.log("welcome subscriber!");
- } else {
-    console.log("goodbye subscriber");
- }
-console.log(`Welcome to AEG Design Im ${name} and im ${age}`);
-const combined = name + age;
-
-console.log(typeof age);
-console.log(typeof combined);
-
-const member1 = 75;
-const member2 = 20;
-
-if (member1 === 75 && member2 === 20) {
-    console.log("higher sales");
-} else {
-    console.log("lowest sales");
+for (name of names) {
+    console.log(name);
+    if (name === "Olivia") {
+        console.log("Olivia is running for President");
+        break;
+    }
 }
 
+let loading = 10;
+
+while(loading < 80) {
+    console.log("game is still loading");
+
+    loading++;
+}
 
