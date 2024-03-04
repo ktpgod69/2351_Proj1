e22b6623ed36bda673783b2a6db7867dc9db16bd
ktpgod69 <henry.fuentes0624@gmail.com>
2024-03-03 19:15:35 -0600

    rock paper scissors

--- app.js
+++ app.js
@@ -1,44 +1,118 @@
-const movielist = ["John Wick", "Scream", "Halloween", "Fury"];
+const game = ()=> {
+    let pScore = 0;
+    let cScore = 0;
 
-const rankings = ["1", "2", "3", "4"];
-console.log(movielist);
-console.log(rankings);
+    //Start the Game
+    const startgame = () => {
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector("match");
 
-movielist.push("Drive");
+        playBtn.addEventListener("click", () => {
+            introScreen.classList.add("fadeout");
+            match.classList.add("fadeIn");
+        });
+    };
+    //Play Match
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerhand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector("computer-hand");
+        const hands = document.querySelectorAll(".hands img");
 
-console.log(movielist);
+        hands.forEach(hand => {
+            hand.addEventListener("animationend", function(){
+                this.style.animation = "";
+            });
+        });
+        //Computer Options
+        const computerOptions = ["rock", "paper", "scissors"];
 
-const John = movielist.indexOf("John Wick");
-console.log(movielist[John]);
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+                //Computer Choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+            
+             setTimeout(() => {
+                //Here is where we compare hands
+                compareHands(this.textContent, computerChoice);
+                //Update Images
+                playerhand,src = "./assets/${this.textcontent}.png";
+                computerHand.src = "./assets/${computerChoice}.png";
+            }, 2000);
+            //Animation
+            playerhand.computedStyleMap.animation = "shakePlayer 2s ease";
+            computerHand.computedStyleMap.animation = "shakecomputer 2s ease";
+            });
+        });
+        };
 
-const user = {
-    name: "Henry",
-    age: 30,
-    married: true,
-    favoritemovies: ["Drive", "Halloween", "It Follows", "Pearl Harbor", "The Mummy"],
-
-    sayName: function() {
-        console.log(this.name);
+        const updateScore = () =>{
+            const playerScore = document.querySelector(".player-score p");
+            const computerScore = document.querySelector(".computer-score p");
+            playerScore.textContent = pScore;
+            computerScore.textContent = cScore;
+        }
+        
+        const compareHands = (playerChoice, computerChoice) =>{
+            //Update Text
+            const winner = document.querySelector(".winner");
+            //Checking for a tie
+            if(playerChoice === computerChoice){
+                winner.textContent = "It is a tie";
+                return;
+            }
+            //Check for rock
+            if(playerChoice === "rock"){
+                if(computerChoice === "scissors"){
+                    winner.textContent = "Player Wins"
+                    pScore++;
+                    updateScore();
+                    return;
+                }else{
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            }
+         }
+        //Check for Paper
+        if(playerChoice === "paper"){
+            if(computerChoice === "scissors"){
+            winner.textContent = "Computer Wins"
+            cScore++;
+            updateScore();
+            return;
+        }else{
+            winner.textContent = "Player Wins";
+            pScore++;
+            updateScore();
+            return;
+        }
     }
+    //Check for Scissors
+    if(playerChoice === "scissors"){
+        if(computerChoice === "rock"){
+            winner.textContent = "Computer Wins"
+            cScore++;
+            updateScore();
+            return;
+        }else{
+            winner.textContent = "Player Wins";
+            pScore++;
+            updateScore();
+            return;
+        }
+    }
+}
+
+//Is call all the inner function
+    startgame();
+    playMatch();
 };
 
-user.sayName();
-
-function sayimmarried() {
-    console.log(`Am i married...true or false? ${true}`);
-}
-
-sayimmarried();
-
-console.log(user.favoritemovies);
-
-function movies() {
-    console.log()
-}
+//start the game fumction
+game();
 
 
-
-//This
-window.apples();
-
-console.log(this);
